12ee798febb9f37c7c09ab1e55e7fc3167bde0cf
Wanderson Luiz <wwluiz17@gmail.com>
2025-08-14 13:08:59 -0300

    CartasSuperTrunfoaventureiro.c

--- CartasSuperTrunfoaventureiro.c
+++ CartasSuperTrunfoaventureiro.c
@@ -2,7 +2,7 @@
 
 int main() {
     // CRIANDO CIDADE 01
-    char estado1[3], codigocarta1[4], nomecidade1[50]; // char do modo string - %c ou %s 
+    char estado1[20], codigocarta1[20], nomecidade1[50]; // char do modo string - %c ou %s 
     int populacao1, pontosturisticos1; // variaveis inteiras - %d
     float area1, pib1, densidadepo1, pibpercapita1; // flutuante - %f
 
@@ -28,15 +28,11 @@ int main() {
     printf("Pontos Turisticos: ");
     scanf("%d", &pontosturisticos1); 
 
-    printf("Densidade Populacional: ");
-    scanf("%f", &densidadepo1); 
+    // Calculando as Variáveis da Cidade 01
+    densidadepo1 = (float) populacao1 / area1;
+    pibpercapita1 = (float) pib1 / populacao1;
 
-     printf("PIB Per Capita: ");
-    scanf("%f", &pibpercapita1); 
-
- 
-
-  // EXIBIÇÃO DAS CARTAS 01
+    // EXIBIÇÃO DAS CARTAS 01
     printf("Código da Carta: %s\n", codigocarta1);
     printf("Estado: %s\n", estado1);
     printf("Nome da Cidade: %s\n", nomecidade1);
@@ -44,12 +40,11 @@ int main() {
     printf("Area: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos Turisticos: %d\n", pontosturisticos1);
-    printf("Densidade Populacional: %f\n", densidadepo1);
-    printf("PIB Per Capita: %f\n", pibpercapita1);
-    
-
+    printf("Densidade populacional: %.2f hab/km²\n", densidadepo1);
+    printf("PIB per capita: %.2f reais\n", pibpercapita1);
+   
     // CRIANDO CIDADE 02
-    char estado2[3], codigocarta2[4], nomecidade2[50]; // char do modo string - %c ou %s 
+    char estado2[20], codigocarta2[20], nomecidade2[50]; // char do modo string - %c ou %s 
     int populacao2, pontosturisticos2; // variaveis inteiras - %d
     float area2, pib2, densidadepo2, pibpercapita2; // flutuante - %f
 
@@ -72,16 +67,12 @@ int main() {
     printf("PIB: ");
     scanf("%f", &pib2); 
 
-  printf("Pontos Turisticos: ");
+    printf("Pontos Turisticos: ");
     scanf("%d", &pontosturisticos2); 
 
-     printf("Densidade Populacional: ");
-    scanf("%f", &densidadepo2); 
-
-     printf("PIB Per Capita: ");
-    scanf("%f", &pibpercapita2); 
-
-  
+    // Calculando as Variáveis da Cidade 02
+    densidadepo2 = (float) populacao2 / area2;
+    pibpercapita2 = (float) pib2 / populacao2;
 
     // EXIBIÇÃO DAS CARTAS 02
     printf("Código da Carta: %s\n", codigocarta2);
@@ -89,11 +80,10 @@ int main() {
     printf("Nome da Cidade: %s\n", nomecidade2);
     printf("Populacao: %d\n", populacao2);
     printf("Area: %.2f\n", area2);
-    printf("PIB: %f\n", densidadepo2);
+    printf("PIB: %.2f\n", pib2);
     printf("Pontos Turisticos: %d\n", pontosturisticos2);
-    printf("Densidade Populacional: %f\n", pibpercapita2);
-    printf("PIB Per Capita: %.2f\n", pib2);
- 
-
+    printf("Densidade populacional: %.2f hab/km²\n", densidadepo2);
+    printf("PIB per capita: %.2f reais\n", pibpercapita2);
+    
     return 0;
 }
